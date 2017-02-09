64ad1470e6260674eb2eb8f548f2114c7916d3f1
NLHawkins <nathanlhawkins@gmail.com>
2017-02-09 13:15:42 -0500

    Add project files.

---
+++ MultiUserBlog_MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MultiUserBlog_MVC.Startup))]
+namespace MultiUserBlog_MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
